5aca4f24d5cd410d1e12c939b7f96112cfe34d2f
utkarsh161099 <47791515+utkarsh161099@users.noreply.github.com>
2022-05-02 12:38:28 +0530

    Update tweak.py

--- tweak.py
+++ tweak.py
@@ -104,19 +104,19 @@ while int(accuracy) < 96 :
 # In[ ]:
 
 
-import smtplib 
-# creates SMTP session 
-s = smtplib.SMTP('smtp.gmail.com', 587) 
-# start TLS for security 
-s.starttls()   
-# Authentication 
-s.login("abc@gmail.com", "*****") 
-# message to be sent 
-message = "Hey Developer, this is to inform you that your MNIST model finally  got the model trained with 99  accuracy "
-# sending the mail 
-s.sendmail("abc@gmail.com", "xyz@gmail.com", message) 
-# terminating the session 
-s.quit()
+# import smtplib 
+# # creates SMTP session 
+# s = smtplib.SMTP('smtp.gmail.com', 587) 
+# # start TLS for security 
+# s.starttls()   
+# # Authentication 
+# s.login("abc@gmail.com", "*****") 
+# # message to be sent 
+# message = "Hey Developer, this is to inform you that your MNIST model finally  got the model trained with 99  accuracy "
+# # sending the mail 
+# s.sendmail("abc@gmail.com", "xyz@gmail.com", message) 
+# # terminating the session 
+# s.quit()
 
 
 # In[ ]:
